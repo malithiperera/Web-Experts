1e011fa5aa9e17e9f14c65e2d2be39fe4b53e1ed
malithiperera <62257561+malithiperera@users.noreply.github.com>
2021-09-15 12:16:02 +0530

    add our product file

---
+++ app/views/view_customer_ourProduct.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Product</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <link rel="stylesheet" href="../../public/styles/view_customer_ourproduct.css">
+</head>
+<body>
+    <div class="body">
+        <div class="header">
+        <?php
+require 'view_headertype2.php';
+?>
+
+        </div>
+        <div class="search">
+    <p>Hello</p>
+</div>
+
+<br>
+<br>
+    <section class="container">
+        
+ <div class="card">
+     <div class="card-image">Image1<img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+ <div class="card">
+     <div class="card-image"><img src="download.jpg" alt=""></div>
+     <h2>Himalee Yoghurd</h2>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry..</p>
+ </div>
+    </section>
+
+
+    <div class="footer1">
+<?php require 'view_footer.php'; ?>
+
+</div>
+</body>
+</html>
