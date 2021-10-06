c125ee9c592d5be69d3d4bc9398658f2dbc0d2bb
ShiranthaKellum <skellumjayasooriya@gmail.com>
2021-10-06 17:53:55 +0530

    Linked manageStocks.js

--- app/views/_1_view_stockmanagerHome.php
+++ app/views/_1_view_stockmanagerHome.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <title>Home </title>
     <link rel="stylesheet" href="style.css">
-    <!-- Boxicons CDN Link -->
+    <!-- Box icons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
