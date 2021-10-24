726bf1673efcb75a04b5ee72bd56604e2ff00a06
SumuduPerera98 <Sharshiperera20@gmail.com>
2021-10-24 16:47:20 +0530

    modify

--- app/views/view_rep_customerRegistration.php
+++ app/views/view_rep_customerRegistration.php
@@ -11,50 +11,50 @@
 </head>
 
 <body>
-<div class="header">
-    <?php
+    <div class="header">
+        <?php
         require 'view_headertype2.php';
         ?>
-    
+
     </div>
     <div class="container">
         <div class="sub-container">
             <form action="../register/register_customer" method="post">
-            <div class="title1">Customer Registration</div>
-            <div class="input-fields">
-            <div class="name">
-            <div class="n1"><label for="FirstName">First Name</label><input type="text" name="FirstName" id="FirstName"
-                class="f">
-            </div>
-            <div class="n2"><label for="LastName">Last Name</label><input type="text" name="LastName" id="LastName"
-                class="l">
-            </div>  
-            </div>
-            </div>
-            <div class="input-fields"><label for="email">Email</label><input type="email" name="email" id="email"
-                    class="inputf">
-            </div>
-            <div class="input-fields"><label for="email">User Id</label><input type="text" name="userid" id="email"
-                    class="inputf">
-            </div>
-            <div class="input-fields"><label for="date">Date of Birth</label><input type="date" name="date" id="date"
-                class="inputf">
-            </div>
-            <div class="input-fields"><label for="address">Address</label><input type="text" name="address" id="address"
-                    class="inputf">
-            </div>
-            <div class="input-fields"><label for="shopName">Shop Name</label><input type="text" name="shopName" id="shopName"
-                    class="inputf">
-            </div>
-            <div class="input-fields"><label for="route">Route</label><input type="text" name="route" id="route"
-                class="inputf">
-        </div>
-        
-            
-            <div class="input-fields"><input type="submit" value="Confirm" id="confirm" name="submit"></div>
+                <div class="title1">Customer Registration</div>
+                <div class="input-fields">
+                    <div class="name">
+                        <div class="n1"><label for="FirstName">First Name</label><input type="text" name="FirstName"
+                                id="FirstName" class="f">
+                        </div>
+                        <div class="n2"><label for="LastName">Last Name</label><input type="text" name="LastName"
+                                id="LastName" class="l">
+                        </div>
+                    </div>
+                </div>
+                <div class="input-fields"><label for="email">Email</label><input type="email" name="email" id="email"
+                        class="inputf">
+                </div>
+                <div class="input-fields"><label for="email">User Id</label><input type="text" name="userid" id="email"
+                        class="inputf">
+                </div>
+                <div class="input-fields"><label for="date">Date of Birth</label><input type="date" name="date"
+                        id="date" class="inputf">
+                </div>
+                <div class="input-fields"><label for="address">Address</label><input type="text" name="address"
+                        id="address" class="inputf">
+                </div>
+                <div class="input-fields"><label for="shopName">Shop Name</label><input type="text" name="shopName"
+                        id="shopName" class="inputf">
+                </div>
+                <div class="input-fields"><label for="route">Route</label><input type="text" name="route" id="route"
+                        class="inputf">
+                </div>
+
+
+                <div class="input-fields"><input type="submit" value="Confirm" id="confirm" name="submit"></div>
         </div>
         </form>
-        
+
     </div>
     <div class="input-fields"><input type="submit" value="Back" id="back" name="submit"></div>
 </body>
--- app/views/view_rep_productList.php
+++ app/views/view_rep_productList.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,174 +8,176 @@
     <title>Product List</title>
     <link rel="stylesheet" href="../../public/styles/view_rep_productList.css">
 </head>
+
 <body>
-<!-- <?php require 'view_headerType2.php';  ?> -->
-<h2>DAILY PRODUCT LIST</h2>
-<div class="table-wrapper">
-    <table class="fl-table">
-        <thead>
-        <tr>
-            <th>Product</th>
-            <th>Quantity</th>
-            
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            
-            <td >Product 1</td>
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-            
-        </tr>
-        <tr>
-            <td >Product 1</td>
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-            
-        </tr>
-        <tr>
-            <td >Product 1</td>
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-            
-        </tr>
-        <tr>
-            <td >Product 1</td>
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-            
-        </tr>
-        <tr>
-            <td >Product 1</td>
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-            
-        </tr>
-        <tr>
-          <td>Product 1</td>
-          <td><input type="text" name="total" id="total" class="inputf"></td>
-          
-      </tr>
-      <tr>
-            <td>Product 1</td>
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-          
-      </tr>
-      <tr>
-            <td>Product 1</td>
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-          
-      </tr>
-      <tr>
-            <td>Product 1</td>
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-          
-      </tr>
-      <tr>
-            <td>Product 1</td> 
-            <td><input type="text" name="total" id="total" class="inputf"></td>
-          
-      </tr>
-      <tr>
-      <td>Product 1</td>
-      <td><input type="text" name="total" id="total" class="inputf"></td>
-        
-    </tr>
-    <tr>
-    <td>Product 1</td>
-    <td><input type="text" name="total" id="total" class="inputf"></td>
-        
-    </tr>
-    <tr>
-    <td>Product 1</td>
-    <td><input type="text" name="total" id="total" class="inputf"></td>
-        
-    </tr>
-    <tr>
-    <td>Product 1</td>
-    <td><input type="text" name="total" id="total" class="inputf"></td>
-        
-    </tr>
-    <tr>
-    <td>Product 1</td>
-    <td><input type="text" name="total" id="total" class="inputf"></td>
-        
-    </tr>
-    <tr>
-    <td>Product 1</td>
-    <td><input type="text" name="total" id="total" class="inputf"></td>
-      
-  </tr>
-  <tr>
-  <td>Product 1</td>
-  <td><input type="text" name="total" id="total" class="inputf"></td>
-      
-  </tr>
-  <tr>
-  <td>Product 1</td>
-  <td><input type="text" name="total" id="total" class="inputf"></td>
-      
-  </tr>
-  <tr>
-  <td>Product 1</td>
-  <td><input type="text" name="total" id="total" class="inputf"></td>
-      
-  </tr>
-  <tr>
-  <td>Product 1</td>
-  <td><input type="text" name="total" id="total" class="inputf"></td>
-      
-  </tr>
-  <tr>
-  <td>Product 1</td>
-  <td><input type="text" name="total" id="total" class="inputf"></td>
-    
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-    
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-    
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-    
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-    
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-  
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-  
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-  
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-  
-</tr>
-<tr>
-<td>Product 1</td>
-<td><input type="text" name="total" id="total" class="inputf"></td>
-  
-</tr>
-        <tbody>
-    </table>
-</div>
-<div class="input-fields"><input type="submit" value="Back" id="back"></div>
-<div class="input-fields"><input type="submit" value="Confirm" id="confirm"></div>     
+    <!-- <?php require 'view_headerType2.php';  ?> -->
+    <h2>DAILY PRODUCT LIST</h2>
+    <div class="table-wrapper">
+        <table class="fl-table">
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Quantity</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+                <tr>
+                    <td>Product 1</td>
+                    <td><input type="text" name="total" id="total" class="inputf"></td>
+
+                </tr>
+            <tbody>
+        </table>
+    </div>
+    <div class="input-fields"><input type="submit" value="Back" id="back"></div>
+    <div class="input-fields"><input type="submit" value="Confirm" id="confirm"></div>
 </body>
+
 </html>
