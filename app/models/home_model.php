2252c65284a95f80ce45a9d3b92d10b6942e6fdb
malithiperera <62257561+malithiperera@users.noreply.github.com>
2021-10-25 08:47:36 +0530

    dashboard customer

--- app/models/home_model.php
+++ app/models/home_model.php
@@ -53,21 +53,67 @@ class home_model extends model
         }
     }
 
-   public function customer_home_detail_check($userid){
-    require '../app/core/database.php';
-    $sql="SELECT * FROM orders WHERE cus_id='$userid' AND status='pending' ";
-    $query="SELECT * FROM delivery WHERE cus_id='$userid'";
-    $sql1=mysqli_query($conn,$sql);
-    $query1=mysqli_query($conn,$query);
-    if($query1==true && $sql1==true )
-    {
-       return array($query1,$sql1);
-    }
-   }
+   
     public function test3(){
         require '../app/core/database.php';
         $sql = "SELECT * FROM product";
         $query = mysqli_query($conn, $sql);
         return $query;
     }
+
+
+
+
+public function customer_card($userid)
+{
+
+    require '../app/core/database.php';
+
+
+
+
+    
+$query="SELECT count(product_id) AS p_count FROM product";
+$result=mysqli_query($conn,$query);
+if($result==true)
+{
+    $data=$result->fetch_array();
+    $input=array("products"=>$data);
+  
+    // return $input;
+}
+
+
+ 
+ 
+// $query="SELECT count(order_id) FROM orders WHERE cus_id='$userid' AND status='pending'";
+// $result=mysqli_query($conn,$query);
+// $data=$result->fetch_array();
+// $input["delievery"]=$data;
+
+// $query="SELECT count(delivery_id) FROM delivery WHERE cus_id='$userid' AND status='pending'";
+// $result=mysqli_query($conn,$query);
+// $data=$result->fetch_array();
+// $input["payment"]=$data;
+
+// return $input;
+
+
+}
+
+public function customer_home_detail_check($userid){
+    require '../app/core/database.php';
+    $sql="SELECT * FROM orders WHERE cus_id='$userid' AND status='pending' ";
+    $sql1="SELECT * FROM delivery  ";
+    $result1=mysqli_query($conn,$sql);
+    $result2=mysqli_query($conn,$sql1);
+
+    if( $result1==true  && $result2==true)
+    {
+
+        // $data=$result1->fetch_array();
+        // $data1=$result2->fetch_array();
+      return array($result1,$result2);
+    }
+   }
 }
