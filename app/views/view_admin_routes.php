d26962b71500933fca3de314e63dd4b0d00a8efc
DinethTharushan <2019cs165@stu.ucsc.lk>
2021-10-30 12:18:41 +0530

    tp pull

--- app/views/view_admin_routes.php
+++ app/views/view_admin_routes.php
@@ -14,7 +14,7 @@
             /* color: #184A78; */
         }
 
-       
+
 
         .content {
             position: absolute;
@@ -84,9 +84,11 @@
             margin-top: 30px;
             margin-bottom: 70px;
         }
-        .buttons p{
+
+        .buttons p {
             color: #184A78;
         }
+
         button {
             width: 100px;
             height: 30px;
@@ -105,6 +107,16 @@
             padding: 10px;
             color: #184A78;
         }
+
+        #suggestion {
+            position: absolute;
+            top: 108px;
+            left:450px;
+            width: 190px;
+            height: 100px;
+            background-color: red;
+            z-index: 10001;
+        }
     </style>
 </head>
 
@@ -119,13 +131,14 @@
             <!-- <button id="edit">EDIT</button> -->
         </div>
 
-        <form class="new_added_row">
+        <div class="new_added_row">
             <!-- <input type="text" name="route_id" placeholder="Route Id" id="route_id" required> -->
-            <input type="text" name="route_id" placeholder="route_id" id="route_id">
+            <!-- <input type="text" name="route_id" placeholder="route_id" id="route_id"> -->
             <input type="text" name="destination" placeholder="Destination" id="destination" required>
             <input type="text" name="route_name" placeholder="Route Name" id="route_name" required>
-            <input type="submit" name="submit">
-        </form>
+            <input type="text" name="rep_id" placeholder="salesrep ID" id="rep_id" onkeyup="suggest_rep()" require>
+            <input type="submit" name="submit" onclick="add_new_route()">
+        </div>
 
         <div class="table">
 
@@ -134,30 +147,109 @@
                     <th>ROUTE ID</th>
                     <th>DESTINATION</th>
                     <th>ROAD</th>
+                    <th>Sales Rep Id</th>
                 </thead>
-                <tbody>
+                <tbody id="content-routes">
 
                 </tbody>
 
 
             </table>
         </div>
+        <div id="suggestion">
+
+        </div>
 
     </div>
 
+
+
     <script>
+        content_route = document.getElementById('content-routes');
+        // route_id = document.getElementById('route_id');
+        destination = document.getElementById('destination');
+        route_name = document.getElementById('route_name');
+        rep_id = document.getElementById('rep_id');
+
+        //suggest sales rep
+        const suggest_rep = () => {
+            type_rep = {
+                rep_id: rep_id.value
+            }
+
+            fetch('http://localhost/web-Experts/public/admin/suggest_rep', {
+                    method: 'POST',
+
+                    headers: {
+                        'Content-Type': 'application/json'
+
+                    },
+
+                    body: JSON.stringify(type_rep)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                });
+            console.log(type_rep);
+        }
+
 
         //get routes
-        const get_routes = () =>{
+        const get_routes = () => {
             fetch('http://localhost/web-Experts/public/admin/get_routes')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
+                .then(response => response.json())
+                .then(data => {
+
+                    for (i = 0; i < data.length; i++) {
+                        content_route.innerHTML += `
+                
+                            <tr>
+                                <td>${data[i]['route_id']}</td>
+                                <td>${data[i]['end']}</td>
+                                <td>${data[i]['route_name']}</td>
+                                <td>${data[i]['rep_id']}</td>
+                            </tr>
+                        
+                        `;
+                    }
+
+
+
+                    console.log(data);
+                })
         }
 
         get_routes();
 
+
+
+
+        //add new route
+        const add_new_route = () => {
+            data_set = {
+                // route_id: route_id.value,
+                destination: destination.value,
+                route_name: route_name.value,
+                rep_id: rep_id
+            }
+
+            fetch('http://localhost/web-Experts/public/admin/add_new_route', {
+                    method: 'POST',
+
+                    headers: {
+                        'Content-Type': 'application/json'
+
+                    },
+
+                    body: JSON.stringify(data_set)
+                })
+                .then(response => response.json())
+                .then(data => {
+
+                });
+
+        }
     </script>
 
 
