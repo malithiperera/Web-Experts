02e4f43a4729690221bc85b9d87b60fd7ded4a4b
DinethTharushan <2019cs165@stu.ucsc.lk>
2021-11-01 09:00:36 +0530

    complete loading process

--- app/views/view_admin_addemployee.php
+++ app/views/view_admin_addemployee.php
@@ -12,6 +12,26 @@ if (!isset($_SESSION['username'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+
+  <style>
+    .loading{
+      position: fixed;
+      top:500px;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+    .loading_message{
+
+    }
+    .loading_message p{
+      color:black;
+    }
+    .main{
+      opacity: 20%;
+    }
+  </style>
+
 </head>
 <link rel="stylesheet" href="../../public/styles/view_admin_addemployee.css">
 
@@ -163,6 +183,10 @@ if (!isset($_SESSION['username'])) {
 
     </div>
   </div>
+
+
+  
+
   <?php
 
   $check = 0;
