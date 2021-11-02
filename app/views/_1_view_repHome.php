a78e9bb6df9e397b769528313c1fcf72484ed853
SumuduPerera98 <Sharshiperera20@gmail.com>
2021-11-02 07:44:47 +0530

    logout

--- app/views/_1_view_repHome.php
+++ app/views/_1_view_repHome.php
@@ -100,7 +100,7 @@ if (!isset($_SESSION['username'])) {
       </li>
 
       <li>
-        <a href="#">
+        <a href="../login/logout">
           <i class="fas fa-sign-out-alt"></i>
           <span class="links_name">Logout</span>
         </a>
