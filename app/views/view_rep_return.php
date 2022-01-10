0c92b732a8824e418e518d335611603f876d056d
SumuduPerera98 <Sharshiperera20@gmail.com>
2022-01-10 17:29:16 +0530

    returns

--- app/views/view_rep_return.php
+++ app/views/view_rep_return.php
@@ -20,7 +20,79 @@
                             <th>Price</th>
                         </tr>
                     </thead>
-                    
+                    <tbody>
+                        <tr>
+                            <td>Product1</td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                        </tr>
+                        <tr>
+                            <td>Product2</td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                        </tr>
+                        <tr>
+                            <td>Product3</td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                        </tr>
+                        <tr>
+                            <td>Product4</td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                        </tr>
+                        <tr>
+                            <td>Product5</td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                        </tr>
+                        <tr>
+                            <td>Product6</td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                        </tr>
+                        <tr>
+                            <td>Product7</td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                        </tr>
+                        <tr>
+                            <td>Product8</td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                        </tr>
+                        <tr>
+                            <td>Product9</td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                        </tr>
+                        <tr>
+                            <td>Product10</td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                        </tr>
+                        <tr>
+                            <td>Product11</td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                        </tr>
+                        <tr>
+                            <td>Product12</td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                        </tr>
+                        <tr>
+                            <td>Product13</td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                            <td><input type="text" name="total" id="total" class="inputf"></td>
+                        </tr>
+                        <tr>
+                            <td>Product14</td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                            <td><input type="text" name="total" id="total" class="inputf1"></td>
+                        </tr>
+                    <tbody>
+                </table>
             </div>
             <div class="submit"><input type="submit" value="Submit" id="submit"></div>
     </body>
